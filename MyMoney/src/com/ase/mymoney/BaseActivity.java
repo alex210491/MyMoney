f961e47fea34ce6b555cbff0a362477aa445b0a6
Alex <Alex@Alex-PC>
2014-04-05 16:49:13 +0300

    init

---
+++ MyMoney/src/com/ase/mymoney/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.ase.mymoney;
+
+import net.simonvt.menudrawer.MenuDrawer;
+import net.simonvt.menudrawer.Position;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.ase.mymoney.ui.SlidingMenuFragment;
+
+public class BaseActivity extends Activity {
+
+	MenuDrawer mMenuDrawer;
+	SlidingMenuFragment mMenuFragment;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mMenuDrawer = MenuDrawer.attach(this, MenuDrawer.Type.BEHIND, Position.LEFT, MenuDrawer.MENU_DRAG_WINDOW);
+		mMenuDrawer.setMenuView(R.layout.menu_frame);
+		
+		mMenuDrawer.setMenuSize((int) this.getResources().getDimension(R.dimen.sliding_menu_width));
+		
+		attachSlidingMenu();
+	}
+	
+	
+	protected void attachSlidingMenu() {
+		mMenuFragment = new SlidingMenuFragment();
+		FragmentTransaction ft = getFragmentManager().beginTransaction().replace(R.id.menuFrame, mMenuFragment);
+		ft.commit();
+	}
+	
+	
+}
---
+++ MyMoney/src/com/ase/mymoney/ui/SlidingMenuFragment.java
@@ -0,0 +1,33 @@
+package com.ase.mymoney.ui;
+
+import com.ase.mymoney.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SlidingMenuFragment extends Fragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View root = inflater.inflate(R.layout.sliding_menu_layout, null);
+		
+		return root;
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+	}
+	
+}
