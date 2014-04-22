7a4f65ab467527398804141cbbfc9b7e3a8244fd
Alex <Alex@Alex-PC>
2014-04-22 22:24:54 +0300

    antonia's layout

--- MyMoney/src/com/ase/mymoney/ui/SlidingMenuFragment.java
+++ MyMoney/src/com/ase/mymoney/ui/SlidingMenuFragment.java
@@ -9,7 +9,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.ase.mymoney.BaseActivity;
 import com.ase.mymoney.R;
 
 public class SlidingMenuFragment extends Fragment {
